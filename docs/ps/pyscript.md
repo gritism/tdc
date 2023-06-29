40e8b1247c5c53b7d172317a5597e7e5506eb004
yoblee <yoblee@gmail.com>
2023-06-29 23:59:40 +0900

    new file:   docs/ps/pys_test.py 	modified:   docs/ps/pyscript.md 	renamed:    docs/ps/pyscript.rst -> docs/ps/pyscript_rst.rst 	modified:   sph_ext/pys/__init__.py 	modified:   sph_ext/pys/__pycache__/__init__.cpython-310.pyc

--- docs/ps/pyscript.md
+++ docs/ps/pyscript.md
@@ -6,42 +6,92 @@ py-config:
   - matplotlib
 ---
 
-# Example with MyST
+# pyscript
 
-## `py-repl` and `py-terminal`
-
-We can create a REPL which will output to a `div` and print `stdout` to a terminal with:
-
-````md
-```{py-repl}
-:output: replOutput
-
-print("hallo world")
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3])
-plt.gcf()
+```{py-config}
+  splashscreen:
+    autoclose: true
+  packages:
+  - matplotlib
 ```
 
-<div id="replOutput"></div>
+## hello world
+### 1) code
+````md
+```{py-repl}
+:output: replOutput_1
 
-```{py-terminal}
+print("hello world")
 ```
 ````
 
-Press `shift+enter` to run the code.
+### 2) result
 
 ```{py-repl}
-:output: replOutput
+:output: replOutput_1
 
-print("hallo world")
+print("hello world")
+```
+
+<div id="replOutput_1"></div>
+
+## basic chart
+### 1) code
+
+````md
+```{py-repl}
+:output: replOutput_2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3])
+plt.gcf()
+```
+````
+
+### 2) result
+* do 'shift+enter' key stroke
+```{py-repl}
+:output: replOutput_2
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3])
 plt.gcf()
 ```
 
-<div id="replOutput"></div>
+<div id="replOutput_2"></div>
 
 ```{py-terminal}
 ```
 
-```{helloworld}
+## py-script
+### 1) code
+````md
+```{py-script}
+:file: pys_test.py
+```
+
+print("Let's compute π:")
+def compute_pi(n):
+    pi = 2
+    for i in range(1,n):
+        pi *= 4 * i ** 2 / (4 * i ** 2 - 1)
+    return pi
+
+pi = compute_pi(100000)
+s = f"π is approximately {pi:.3f}"
+print(s)
+````
+
+### 2) result
+
+```{literalinclude} pys_test.py
+:language: python
+```
+
+```{py-script}
+:file: pys_test.py
+:output: replOutput_3
+```
+
+<div id="replOutput_3"></div>
+
+```{py-terminal}
+```
--- sph_ext/pys/__init__.py
+++ sph_ext/pys/__init__.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import json
 from pathlib import Path
 
@@ -8,74 +10,83 @@ from sphinx.application import Sphinx
 from sphinx.util.docutils import SphinxDirective
 from sphinx.util.logging import getLogger
 
+logger = getLogger(__name__)
+
 __version__ = "2023.06.1"
-PYSCRIPT_VERSION = "2023.05.1"
 
-LOGGER = getLogger(__name__)
+# PYSCRIPT_VERSION = "2023.05.1"
 
-def setup(app: Sphinx):
-    '''sphinx config setup'''
-    PYSCRIPT_JS_URL = f"https://pyscript.net/releases/{PYSCRIPT_VERSION}/pyscript.js"
-    PYSCRIPT_CSS_URL = f"https://pyscript.net/releases/{PYSCRIPT_VERSION}/pyscript.css"
-
-    app.add_config_value(
-        "pys_js", PYSCRIPT_JS_URL, "env"
-    )
-    app.add_config_value(
-        "pys_css", PYSCRIPT_CSS_URL, "env"
-    )
-
-    '''register directive for pyscript elements'''
-    app.add_directive("py-config", PyConfig)
-    app.add_directive("py-script", PyScript)
-    app.add_directive("py-repl", PyRepl)
-    app.add_directive("py-terminal", PyTerminal)
-
-    '''define callback function'''
-    app.connect("doctree-read", doctree_read)
-    app.connect("html-page-context", add_pyscripts)
-
-    return {
-        'version': __version__,
-        'parallel_read_safe': True,
-        'parallel_write_safe': True,
-    }
-
+PYSCRIPT_VERSION: str = None
 
 class PyConfig(SphinxDirective):
 
-    # this enables content in the directive
     has_content = True
 
+    print("PyConfig #################################")
     # Must override run() is subclass.
+    # metadata = app.env.metadata[pagename]
     def run(self):
-        """Parse the config"""
+
+        print("PyConfig content #################", self.content)
         if self.content:
             try:
-                temp = "\n".join(self.content)
-                config = yaml.safe_load("\n".join(self.content))
-            except Exception:
-                raise self.error("Could not read config as YAML")
+                yml_content = "\n".join(self.content)
+                py_config = yaml.safe_load(yml_content)
+            except Exception as e:
+                logger.error(f'py-config contents is not like YAML \n {e}')
         else:
-            config = {}
+            py_config = {}
 
-        self.env.metadata[self.env.docname]["py-config"] = json.dumps(config)
-        LOGGER.info(f'PyConfig {self.content}')
-        LOGGER.info(f'PyConfig \n{temp}')
-        LOGGER.info(f'PyConfig {self.env.docname}')
-        LOGGER.info(f'PyConfig {self.env.metadata}')
+        self.env.metadata[self.env.docname]["py-config"] = json.dumps(py_config)
+        logger.debug(f'################# metadata {self.env.metadata}')
 
-        return []
+        data_str = json.dumps(py_config, indent=2)
+
+        # return []
+        return [nodes.raw(
+                    "",
+                    f'<py-config type="json">\n{data_str}\n</py-config>\n',
+                    format="html",
+                )]
+    
+class PyRepl(SphinxDirective):
+
+    has_content = True
+    option_spec = {
+        "auto-generate": directives.unchanged,
+        "output": directives.unchanged,
+        "src": directives.path,
+    }
+
+    def run(self):
+        print("PyRepl #################################")
+        attrs: str = ""
+        code: str = ""
+
+        for key, value in self.options.items():
+            attrs += f' {key}="{value}"'
+
+        if self.content:
+            code = "\n".join(self.content)
+
+        py_repl = f'''
+            <py-repl {attrs}>
+            {code}
+            </py-repl>
+        '''
+        # logger.info(py_repl)
+        return [nodes.raw("", py_repl, format="html")]
 
 class PyScript(SphinxDirective):
 
     has_content = True
     option_spec = {
         "file": directives.path,
+        "output": directives.unchanged
     }
 
     def run(self):
-        """Add the py-script tag"""
+
         if "file" in self.options:
             path = Path(self.env.relfn2path(self.options["file"])[1])
             try:
@@ -89,77 +100,54 @@ class PyScript(SphinxDirective):
             raise self.error("Must provide either content or the 'file' option")
         return [nodes.raw("", f"<py-script>\n{code}\n</py-script>\n", format="html")]
     
-class PyRepl(SphinxDirective):
-
-    has_content = True
-    option_spec = {
-        "auto-generate": directives.flag,
-        "output": directives.unchanged,
-        "src": directives.path,
-    }
-
-    def run(self):
-        """Add the py-repl tag"""
-        attrs = ""
-        code = ""
-        if "auto-generate" in self.options:
-            attrs += ' auto-generate="true"'
-        if "output" in self.options:
-            attrs += f' output="{self.options["output"]}"'
-        if "src" in self.options:
-            attrs += f' src="{self.options["src"]}"'
-        if self.content:
-            code = "\n".join(self.content)
-
-        LOGGER.info(f'self.options \n{self.options}')
-        '''raw(Special, Inline, PreBibliographic, FixedTextElement)'''
-        return [nodes.raw("", f"<py-repl{attrs}>\n{code}\n</py-repl>\n", format="html")]
-
 class PyTerminal(SphinxDirective):
-
-    """Add a py-terminal tag"""
 
     option_spec = {
         "auto": directives.flag,
+        "false": directives.flag
     }
 
     def run(self):
-        """Add the py-terminal tag"""
-        attrs = ""
-        if "auto" in self.options:
-            attrs += " auto"
-        return [nodes.raw("", f"<py-terminal{attrs}></py-terminal>\n", format="html")]
+        attrs: str = ""
 
-def add_pyscripts(
-    app: Sphinx, pagename: str, templatename: str, context, doctree: nodes.document
-):
-    """Add extra variables to the HTML template context."""
-    if doctree and "pyscript" in doctree:
+        for key, _ in self.options.items():
+            attrs += f' {key}'
+
+        py_terminal = f'''
+            <py-terminal {attrs}></py-terminal>
+        '''
+
+        return [nodes.raw("", py_terminal, format="html")]
+
+def _add_pyscripts(app: Sphinx, pagename: str, templatename: str, context, doctree: nodes.document):
+
+    metadata = app.env.metadata[pagename]
+    if "py-config" in metadata:
+        print(metadata)
         app.add_js_file(app.config.pys_js, loading_method="defer")
         app.add_css_file(app.config.pys_css)
 
-def doctree_read(app: Sphinx, doctree: nodes.document):
-    """Setup the doctree."""
-    metadata = app.env.metadata[app.env.docname]
-    if "py-config" in metadata:
-        try:
-            data = json.loads(metadata["py-config"])
-            assert isinstance(data, dict), "must be a dictionary"
-        except Exception as exc:
-            LOGGER.warning(
-                f"Could not parse pyscript config: {exc}", location=(app.env.docname, 0)
-            )
-        else:
-            doctree["pyscript"] = True
-            data_str = json.dumps(data, indent=2)
-            doctree.append(
-                nodes.raw(
-                    "",
-                    f'<py-config type="json">\n{data_str}\n</py-config>\n',
-                    format="html",
-                )
-            )
+def setup(app: Sphinx) -> dict[str, Any]:
 
-if __name__ == '__main__':
-    sph = Sphinx('./docs', './docs', './docs/_build', './', 'html')
-    setup(sph)
+    if PYSCRIPT_VERSION is None:
+        PYSCRIPT_JS_URL = f"https://pyscript.net/latest/pyscript.js"
+        PYSCRIPT_CSS_URL = f"https://pyscript.net/latest/pyscript.css"
+    else:
+        PYSCRIPT_JS_URL = f"https://pyscript.net/releases/{PYSCRIPT_VERSION}/pyscript.js"
+        PYSCRIPT_CSS_URL = f"https://pyscript.net/releases/{PYSCRIPT_VERSION}/pyscript.css"
+
+    app.add_config_value("pys_js", PYSCRIPT_JS_URL, "env")
+    app.add_config_value("pys_css", PYSCRIPT_CSS_URL, "env")
+
+    app.add_directive("py-config", PyConfig)
+    app.add_directive("py-script", PyScript)
+    app.add_directive("py-repl", PyRepl)
+    app.add_directive("py-terminal", PyTerminal)
+
+    app.connect("html-page-context", _add_pyscripts)
+
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
